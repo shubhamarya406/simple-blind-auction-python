b3442991da81e3150c0add0340b36829bb97620c
Shubham Arya <shubhamarya406@gmail.com>
2021-03-28 04:12:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from replit import clear
+from art import logo
+#HINT: You can call clear() to clear the output in the console.
+print(logo)
+print("Welcome to the secret auction program!!!")
+choice="yes"
+bidder={}
+while choice.lower()=="yes":
+  name=input("\nWhat is your name?:")
+  bidder[name]=int(input("What is your bid?: $"))
+  choice=input("Are ther any other bidders? Type 'yes' or 'no'.\n")
+  if choice=="yes":
+    clear()
+max=0
+winner=""
+for i in bidder:
+  if bidder[i]>=max:
+    max=bidder[i]
+    winner=i
+clear()
+print(f"The winner is {winner} with a bid of ${max}.")
